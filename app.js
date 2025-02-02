6d55acbbb572d31fa752eafc20f6ac60da0941ba
dweigert777 <dweigert1@gmail.com>
2025-02-02 17:34:39 -0300

    Versão funcional do projeto

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    const inputAmigo = document.getElementById('amigo');
+    const nomeAmigo = inputAmigo.value.trim();
+
+    if (nomeAmigo ==='') {
+        alert('Por favor, insira um nome');
+        return;
+    }
+    amigos.push(nomeAmigo);
+    atualizarLista();
+    inputAmigo.value = '';
+    inputAmigo.focus();
+
+  
+}
+
+function atualizarLista() {
+    const listaAmigoUl = document.getElementById('listaAmigos');
+    listaAmigoUl.innerHTML = '';
+
+    amigos.forEach(amigo => {
+        const li =document.createElement('li');
+        li.textContent = amigo;
+        listaAmigoUl.appendChild(li);
+
+    });
+}
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('Não há amigos para sortear. Insira nomes antes de sortear.');
+        return;
+    }
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+    const resultadoUl = document.getElementById('resultado');
+    resultadoUl.innerHTML = `<li>${amigoSorteado}<li>`;
+}
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('button-add').addEventListener('click', adicionarAmigo)
+    document.getElementById('button-draw').addEventListener('click', sortearAmigo)
+});
